e0ce0ee1b9314016902fcad6a7722e49e30b11b4
mountainwen <12750432@gmail.com>
2023-11-21 10:40:20 +0800

    week10

---
+++ week10/week10-6.py
@@ -0,0 +1,8 @@
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        
+        total=sum(salary)-max(salary)-min(salary)
+        N=len(salary)-2#因為扣掉最大值,最小值,
+        return total/N
+  
+        #return sum(salary)-max(salary)-min(salary) / (len(salary)-2)
